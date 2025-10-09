33cca8e038077b42356d58f86e9a56d0793cd753
SamraddhiGupta11 <samraddhigupta208011@gmail.com>
2025-10-09 16:08:11 +0530

    Create 974subarraysums divisible by k.cpp

---
+++ 974subarraysums divisible by k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int sum=0,count=0;
+        mp[0]=1;
+        for(int num:nums){
+            sum+=num;
+            int rem=sum%k;
+            if(rem<0){
+                rem+=k;
+            }
+            if(mp.find(rem)!=mp.end()){
+                count+=mp[rem];
+            }
+            mp[rem]++;
+        }
+        return count;
+    }
+};
